c67d75b71dfa6f4dce66b6ea695c52a4a59db954
Youngwoo Seo <devsyw@gmail.com>
2021-08-06 19:20:38 +0900

    날씨앱 만들기 중간커밋

--- App.js
+++ App.js
@@ -2,18 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import Loading from "./Loading";
+import axios from "axios";
 import * as Location from "expo-location";
+i
+
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   }
 
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  }
+
   getLocation = async() => {
     try {
-      const respose = await Location.requestPermissionsAsync();
-      console.log(respose);
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+
+      this.getWeather(latitude, longitude);
       this.setState({isLoading: false});
 
       console.log(latitude, longitude);
@@ -21,7 +34,6 @@ export default class extends React.Component {
     } catch(error) {
       Alert.alert("당신을 찾을수 없습니다!", "ㅠㅠ");
     }
-
   }
 
   componentDidMount() {
